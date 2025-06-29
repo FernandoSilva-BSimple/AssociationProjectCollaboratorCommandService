e782bff27e1a3c237e439174fc3754eafcee2a24
Fernando Silva <fernandosilvajfgs@gmail.com>
2025-06-30 00:41:05 +0100

    fix tests

--- WebApi.Tests/Consumers/CollaboratorCreatedConsumerTests.cs
+++ WebApi.Tests/Consumers/CollaboratorCreatedConsumerTests.cs
@@ -1,21 +1,27 @@
 using Application.Interfaces;
 using Domain.Messages;
+using Domain.Models;
 using MassTransit;
 using Moq;
 public class CollaboratorCreatedConsumerTests
-{/*
+{
     [Fact]
     public async Task Should_handle_collaborator_created_message()
     {
+
+        var initDate = DateTime.Now;
+        var endDate = DateTime.Now.AddDays(1);
+        PeriodDateTime periodDateTime = new PeriodDateTime(initDate, endDate);
+
         var service = new Mock<ICollaboratorService>();
         var consumer = new CollaboratorCreatedConsumer(service.Object);
-        var message = new CollaboratorCreated(Guid.NewGuid());
+        var message = new CollaboratorCreatedMessage(Guid.NewGuid(), Guid.NewGuid(), periodDateTime);
 
-        var context = Mock.Of<ConsumeContext<CollaboratorCreated>>(c => c.Message == message);
+        var context = Mock.Of<ConsumeContext<CollaboratorCreatedMessage>>(c => c.Message == message);
 
         await consumer.Consume(context);
 
-        service.Verify(s => s.SubmitAsync(message.id), Times.Once);
+        service.Verify(s => s.SubmitAsync(message.Id), Times.Once);
     }
-    */
+
 }
--- WebApi.Tests/Consumers/ProjectCreatedConsumerTests.cs
+++ WebApi.Tests/Consumers/ProjectCreatedConsumerTests.cs
@@ -1,23 +1,28 @@
 using Application.Interfaces;
 using Domain.Messages;
+using Domain.Models;
 using MassTransit;
 using Moq;
 using WebApi.Consumers;
 
-/*public class ProjectCreatedConsumerTests
+public class ProjectCreatedConsumerTests
 {
     [Fact]
     public async Task Should_handle_project_created_message()
     {
+
+        var initDate = DateOnly.FromDateTime(DateTime.Now);
+        var endDate = DateOnly.FromDateTime(DateTime.Now.AddDays(1));
+        PeriodDate periodDate = new PeriodDate(initDate, endDate);
+
         var service = new Mock<IProjectService>();
         var consumer = new ProjectCreatedConsumer(service.Object);
-        var message = new ProjectCreatedMessage(Guid.NewGuid());
+        var message = new ProjectCreatedMessage(Guid.NewGuid(), "Projeto 1", "P1", periodDate);
 
-        var context = Mock.Of<ConsumeContext<ProjectCreated>>(c => c.Message == message);
+        var context = Mock.Of<ConsumeContext<ProjectCreatedMessage>>(c => c.Message == message);
 
         await consumer.Consume(context);
 
         service.Verify(s => s.SubmitAsync(message.id), Times.Once);
     }
 }
-*/
